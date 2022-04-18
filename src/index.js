3c1682df2fcbcb480443ce92c4eec6db7de0bc7b
everyx <lunt.luo@gmail.com>
2022-04-18 22:12:52 +0800

    refactor: remove tencen and crypt functins
    Release-As: 2.1.0

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ const defaultParams = {
   width: 0,
   height: 0,
   format: "auto",
+  // @deprecated [use format instead]
+  extension: "auto",
   gravity: "ce",
+  // @deprecated [use gravity instead]
+  // extend: "ce",
   quality: 80,
   enlarge: 1,
   resizing_type: "fill",
@@ -18,8 +22,25 @@ function Init(baseUrl) {
   const url = new URL(baseUrl);
 
   this.getNewUrl = function (obj) {
-    const params = Object.assign({}, defaultParams);
-    Object.assign(params, obj);
+    const params = { ...defaultParams, ...obj };
+
+    if (params.url && typeof params.url === "string") {
+      return;
+    }
+
+    // backward compatibility
+    if (params.extension && params.extension !== "auto" && params.format === "auto") {
+      params.format = params.extension;
+    }
+
+    const gravityTypes = ["no", "so", "ea", "we", "noea", "nowe", "soea", "sowe", "ce"];
+    if (params.extend && gravityTypes.includes(params.extend.split.split(":")[0]) && params.gravity === "ce") {
+      params.gravity = params.extend;
+    }
+
+    if (params.format === "auto") {
+      params.format = params.url.split(".").pop();
+    }
 
     const urlSafeBase64 = string => {
       return Buffer.from(string)
@@ -28,145 +49,14 @@ function Init(baseUrl) {
         .replace(/\+/g, "-")
         .replace(/\//g, "_");
     };
+    
+    const sourcePathname = urlSafeBase64(new URL(params.url).pathname);
+    const processingOptions = `pr:sharp/rs:${params.resizing_type}:${params.width}:${params.height}:${params.enlarge}:${params.extend}/g:${params.gravity}/q:${params.quality}`;
 
-    if (params.url && typeof params.url === "string") {
-      if (params.format === "auto") {
-        params.format = params.url.split(".").pop();
-      }
+    url.pathname = `/${processingOptions}/${sourcePathname}.${params.format}`;
 
-      const sourcePathname = urlSafeBase64(new URL(params.url).pathname);
-      const processingOptions = `pr:sharp/rs:${params.resizing_type}:${params.width}:${params.height}:${params.enlarge}:${params.extend}/g:${params.gravity}/q:${params.quality}`;
-
-      url.pathname = `/${processingOptions}/${sourcePathname}.${params.format}`;
-
-      return url.toString();
-    }
+    return url.toString();
   }
 }
 
-function CryptographicInit(init) {
-  const initDefault = {
-    salt: "",
-    key: "",
-    baseUrl: ""
-  }
-
-  Object.assign(initDefault, init);
-
-  const createHmac = require("create-hmac");
-
-  const urlSafeBase64 = string => {
-    return Buffer.from(string)
-      .toString("base64")
-      .replace(/=/g, "")
-      .replace(/\+/g, "-")
-      .replace(/\//g, "_");
-  };
-
-  const hexDecode = hex => Buffer.from(hex, "hex");
-
-  const sign = (salt, target, secret) => {
-    const hmac = createHmac("sha256", hexDecode(secret));
-    hmac.update(hexDecode(salt));
-    hmac.update(target);
-    return urlSafeBase64(hmac.digest());
-  };
-
-  this.getNewUrl = function (obj) {
-    const params = Object.assign({}, defaultParams);
-    Object.assign(params, obj);
-
-    if (params.url && typeof params.url === "string") {
-      if (params.format === "auto") {
-        params.format = params.url.split(".").pop();
-      }
-
-      const sourcePathname = new URL(params.url).pathname;
-      const encodedSourceUrl = urlSafeBase64(sourcePathname);
-
-      const processingOptions = `rs:${params.resizing_type}:${params.width}:${params.height}:${params.enlarge}/g:${params.gravity}/q:${params.quality}`;
-
-      const target = `/${processingOptions}/${encodedSourceUrl}.${params.format}`;
-      const signature = sign(
-        initDefault.salt,
-        target,
-        initDefault.key
-      );
-
-      const url = new URL(initDefault.baseUrl);
-      url.pathname = `/${signature}/${processingOptions}/${encodedSourceUrl}.${params.format}`;
-      const result = url.toString();
-
-      return result;
-    }
-  }
-}
-
-function TencentInit(init) {
-  const initDefault = {
-    baseUrl: null,
-    bucket: "examplebucket-1250000000",
-    region: "ap-shanghai",
-    protocol: "http",
-    key: "imageMogr2"
-  }
-
-  const gravity = {
-    no: "north",
-    so: "south",
-    ea: "east",
-    we: "west",
-    noea: "northeast",
-    nowe: "northwest",
-    soea: "southeast",
-    sowe: "southwest",
-    ce: "center"
-  }
-
-  Object.assign(initDefault, init);
-
-  const url = initDefault.baseUrl ? new URL(initDefault.baseUrl) : new URL(`${initDefault.protocol}://${initDefault.bucket}.cos.${initDefault.region}.myqcloud.com`);
-
-  this.getNewUrl = function (obj) {
-    const params = Object.assign({}, defaultParams);
-    Object.assign(params, obj);
-
-    if (params.url && typeof params.url === "string") {
-      url.pathname = new URL(params.url).pathname
-
-      let search = `?${initDefault.key}`
-
-      if (params.width && !params.height) {
-        search += `/thumbnail/${params.width}x`
-      }
-
-      if (!params.width && params.height) {
-        search += `/thumbnail/x${params.height}`
-      }
-
-      if (params.width && params.height) {
-        search += `/crop/${params.width}x${params.height}`
-      }
-
-      if (params.gravity) {
-        search += `/gravity/${gravity[params.gravity]}`
-      }
-
-      if (params.quality) {
-        search += `/quality/${params.quality}`
-      }
-
-      if (params.format !== "auto") {
-        search += `/format/${params.format}`
-      }
-
-      if (search) {  
-        url.search = search
-      }
-
-      return url.toString();
-    }
-  }
-}
-
-module.exports = { Init, CryptographicInit, TencentInit };
+module.exports = { Init };
